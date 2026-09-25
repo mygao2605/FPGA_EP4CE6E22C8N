3ee7456d85005fcf153299ce4584176b649f80c0
mygao2605 <tuna38@viettel.com.vn>
2026-09-25 09:34:28 +0700

    led

--- L1.LED/led.v
+++ L1.LED/led.v
@@ -1,5 +1,11 @@
 module led (
-	output wire LED	
+	output wire LED1, //84
+	output wire LED2, //85
+	output wire LED3, //86
+	output wire LED4 //87
 );
-assign LED= 1'b0;
+assign LED1 = 1'b0;
+assign LED2 = 1'b1;
+assign LED3 = 1'b0;
+assign LED4 = 1'b1;
 endmodule
